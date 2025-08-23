6c18ed9ed555fa16ce05550e5388878302a5fd24
KaChuDomicilio <kachuabarrotes@gmail.com>
2025-08-23 16:38:10 -0600

    reploy

--- api/data/categorias.js
+++ api/data/categorias.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         return res.status(200).json({ categories: [] });
       }
     }
-
+    
     if (req.method === 'PUT') {
       let body = req.body;
       if (typeof body === 'string') try { body = JSON.parse(body); } catch {}
